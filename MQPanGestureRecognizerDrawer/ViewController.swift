2b3a03137220dfc33bd4044e3b9a908297ed0ec8
文静 <wenjing@txdev.org>
2016-08-01 10:27:19 +0800

    取消使用KVO监听，使用常规判断，另添加缩放功能

--- MQPanGestureRecognizerDrawer/ViewController.swift
+++ MQPanGestureRecognizerDrawer/ViewController.swift
@@ -26,12 +26,12 @@ class ViewController: UIViewController {
         let pan = UIPanGestureRecognizer(target: self, action: #selector(panClick))
         view.addGestureRecognizer(pan)
         
-        /// 使用 KVO 实时监听 mainView 的 frame 属性
-        ///
-        /// - parameter forKeyPath: 观察者（谁想监听）
-        /// - parameter options:    监听的属性
-        /// - parameter context:    监听新值的改变
-        mainView.addObserver(self, forKeyPath: "frame", options: NSKeyValueObservingOptions.New, context: nil)
+//        /// 使用 KVO 实时监听 mainView 的 frame 属性（比较耗性能）
+//        ///
+//        /// - parameter forKeyPath: 观察者（谁想监听）
+//        /// - parameter options:    监听的属性
+//        /// - parameter context:    监听新值的改变
+//        mainView.addObserver(self, forKeyPath: "frame", options: NSKeyValueObservingOptions.New, context: nil)
     }
     
     /// 只要监听到属性一改变，就会调用观察者的此方法，通知有新值
@@ -45,25 +45,61 @@ class ViewController: UIViewController {
         }
     }
     
-    // 移除观察者
-    deinit{
-        mainView.removeObserver(self, forKeyPath: "frame")
-    }
+//    // 移除观察者
+//    deinit{
+//        mainView.removeObserver(self, forKeyPath: "frame")
+//    }
     
     func panClick(pan:UIPanGestureRecognizer){
         
         // 获取移动位置
         let transP = pan.translationInView(view)
         mainView.frame = frameWithOffsetX(transP.x)
+        
+        // (不使用KVO)判断 mainView 的X 是否 >0
+        observeValueForKeyPath(nil, ofObject: nil, change: nil, context: nil)
+        
         // 复位
         pan.setTranslation(CGPoint.zero, inView: view)
         
     }
     
     /// 根据偏移 x 重新设置 mainView.frame
+    /// 手指往右移，视图X轴也要右移（x++），Y轴往下移（y增加）,尺寸缩放（按比例）
     func frameWithOffsetX(offsetx:CGFloat)->CGRect{
+        
+        // 获取上一次的frame
         var mainF = mainView.frame
+        
+        // mainView 下移的最大值
+        let maxY:CGFloat = 80.0
+        let screenH = UIScreen.mainScreen().bounds.size.height
+        
+        // X 轴每平移一点，Y轴也需要移动
+        let offsetY = offsetx * maxY / screenH
+        
+        // 获取上一次的高度
+        let preH = mainF.size.height
+        let preW = mainF.size.width
+        
+        // 获取当前高度
+        var currentH = preH - 2 * offsetY
+        
+        if mainF.origin.x < 0 { // 左移
+            currentH = preH + 2 * offsetY
+        }
+        
+        // 获取缩放比例
+        let scale = currentH / preH
+
+        // 获取当前宽度
+        let currentW = preW * scale
+        // 获取当前X
         mainF.origin.x += offsetx
+        mainF.origin.y = (screenH - currentH) * 0.5
+        
+        mainF.size.height = currentH
+        mainF.size.width = currentW
         return mainF
     }
     
